bc57a408f9ffcac2b96083e7b675baa749e08e5c
Trond Albinussen <trond.albinussen@blinq.no>
2023-04-13 13:19:08 +0200

    Created 2 components

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,12 @@
+import MuiBadge from '@mui/material/Badge';
+
+/** *
+ * ```js
+ * import { Badge } from '@toll/ui5';
+ * ```
+ * https://mui.com/material-ui/api/badge
+ */
+
+export const Badge = MuiBadge;
+
+export default Badge;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import MuiButton from '@mui/material/Button';
+
+/** *
+ * ```js
+ * import { Button } from '@toll/ui5';
+ * ```
+ * https://mui.com/material-ui/api/button
+ */
+
+export const Button = MuiButton;
+
+export default Button;
